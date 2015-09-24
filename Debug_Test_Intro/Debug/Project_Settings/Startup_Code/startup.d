a46f1a54434aa8379a9454cce0235895a0056f4f
Cornel Emmenegger <cornel.emmenegger@gmx.ch>
2015-09-24 13:09:42 +0200

    gitignore

---
+++ Debug_Test_Intro/Debug/Project_Settings/Startup_Code/startup.d
@@ -0,0 +1,8 @@
+Project_Settings/Startup_Code/startup.o: \
+ ../Project_Settings/Startup_Code/startup.c \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/IO_Map.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Static_Code/IO_Map/MKL25Z4.h
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/IO_Map.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Static_Code/IO_Map/MKL25Z4.h:
---
+++ Debug_Test_Intro/Debug/Sources/Events.d
@@ -0,0 +1,76 @@
+Sources/Events.o: ../Sources/Events.c \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/Cpu.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Types.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Error.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Const.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/IO_Map.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Static_Code/IO_Map/MKL25Z4.h \
+ ../Sources/Events.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Types.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Error.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Const.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/IO_Map.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LED1.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LEDpin1.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/BitIoLdd1.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Static_Code/PDD/GPIO_PDD.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Static_Code/PDD/PDD_Types.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/Cpu.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LEDpin1.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/BitIoLdd1.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/WAIT1.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LED2.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LEDpin2.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/BitIoLdd2.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LEDpin2.h \
+ C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/BitIoLdd2.h
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/Cpu.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Types.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Error.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Const.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/IO_Map.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Static_Code/IO_Map/MKL25Z4.h:
+
+../Sources/Events.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Types.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Error.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/PE_Const.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/IO_Map.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LED1.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LEDpin1.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/BitIoLdd1.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Static_Code/PDD/GPIO_PDD.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Static_Code/PDD/PDD_Types.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/Cpu.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LEDpin1.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/BitIoLdd1.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/WAIT1.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LED2.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LEDpin2.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/BitIoLdd2.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/LEDpin2.h:
+
+C:/Users/Cornel/Documents/Debug_Test_Intro/Generated_Code/BitIoLdd2.h:
