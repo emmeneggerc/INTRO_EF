51488d599a41e06c8d7b1d645dd51f23bca7ba39
itblinks <marioandrea.fischer@stud.hslu.ch>
2015-10-30 15:14:56 +0100

    added trigger files

---
+++ Common_Files/Trigger.h
@@ -0,0 +1,13 @@
+/*
+ * Trigger.h
+ *
+ *  Created on: 30.10.2015
+ *      Author: Mario
+ */
+
+#ifndef SOURCES_COMMON_FILES_TRIGGER_H_
+#define SOURCES_COMMON_FILES_TRIGGER_H_
+
+
+
+#endif /* SOURCES_COMMON_FILES_TRIGGER_H_ */
